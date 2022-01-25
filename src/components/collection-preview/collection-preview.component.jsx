104818be5ffe6564f7c5552702ca1923913da436
AyariAla <alaaeddine.ayari1@esprit.tn>
2022-01-25 17:23:56 +0100

    updated the app with shop page, collection preview comp and collectionitem comp

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,25 +1,21 @@
 import React from 'react';
 
+import CollectionItem from '../collection.item/collection-item.component';
 import './collection-preview.styles.scss';
 
-const CollectionPreview = ({ title,items }) => (
+
+// /shop page collections Hats, Sneakers etc by 4
+const CollectionPreview = ({ title, items }) => (
     <div className='collection-preview'>
-        <h1 className='title'>{title.toUpperCase()}</h1>
-            <div className='collection-preview'>
-                <div className='preview'>
-                    {items
-                        .filter((item, idx ) => idx < 4) //filtering items by 4
-                        .map((item) => (
-                                <div key={item.id}>{item.name}</div>
-                            ))                        
-                                /* a bunch of annonymous fcts gets re-rendered after every call for the component
-                                items.map((item) => (
-                                <div key={item.id}>{item.name}</div>
-                            ))
-                                */                       
-                    }
-                </div>
-            </div>
+        <h1 className='title'>{title.toUpperCase()}</h1>    
+            <div className='preview'>
+                 {items
+                    .filter((item, idx ) => idx < 4) //filtering items by 4
+                    .map(({ id, ...otherItemProps }) => (
+                                <CollectionItem key={id} {...otherItemProps} />
+                     ))                        
+                 }
+            </div>         
     </div>
 );
 export default CollectionPreview;
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -6,10 +6,12 @@ import './menu-item.styles.scss'
 const MenuItem= ({title, imageUrl, size, history, linkUrl, match }) => (
     <div 
     className={`${size} menu-item`} //string interpolation
-    onClick={() => history.push(`${match.url}${linkUrl}`)}>
-      <div className='background-image' style={{
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+    <div className='background-image' style={{
         backgroundImage: `url(${imageUrl})`
-      }} />
+      }} 
+    />
     <div className='content'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <span className='subtitle'>SHOP NOW</span>
