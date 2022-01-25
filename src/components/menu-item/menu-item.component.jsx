acb1d4d1b1609b627b9d6522ecb1e0b56dfc2f6e
AyariAla <alaaeddine.ayari1@esprit.tn>
2022-01-25 12:01:56 +0100

    broken homepage into components, and finish styling menu-item component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,13 +4,13 @@ import './menu-item.styles.scss'
 
 const MenuItem= ({title, imageUrl, size }) => (
     <div 
-    style={{
-      backgroundImage: `url(${imageUrl})`/* Passing the image as a bgrd for styling*/
-    }} 
     className={`${size} menu-item`} //string interpolation
     >
+      <div className='background-image' style={{
+        backgroundImage: `url(${imageUrl})`
+      }} />
     <div className='content'>
-        <h1 className='title'>{title}</h1>
+        <h1 className='title'>{title.toUpperCase()}</h1>
         <span className='subtitle'>SHOP NOW</span>
     </div>
   </div>
