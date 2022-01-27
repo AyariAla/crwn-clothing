9ccb1b9e25c521bf64e2ecdb5c99dd00bb3c13bb
AyariAla <alaaeddine.ayari1@esprit.tn>
2022-01-27 16:05:44 +0100

    finish auth, user data is stored in db after sign up w/ ggle and integrated auth state management

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,7 +12,7 @@ const config = {
     measurementId: "G-BGJYYHZDKX"
 };
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
@@ -21,4 +21,35 @@ const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account'});
 
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+
+//
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if(!userAuth) return; //if no user then backoff
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+  
+  if(!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+   try {
+     await userRef.set({
+       displayName, 
+       email,
+       createdAt,
+       ...additionalData
+     })
+   } catch (error) {
+     console.log('error creating user', error.message);
+   }
+  }
+
+  return userRef;
+
+};
+
+
+
 export default firebase;
