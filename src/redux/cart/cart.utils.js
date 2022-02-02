dd2635c09d29528f24c743d84a856057a264bd11
AyariAla <alaaeddine.ayari1@esprit.tn>
2022-02-02 18:54:00 +0100

    added addItem and removeItem functionnality to checkout Item comp

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -12,3 +12,22 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 	}
 	return [...cartItems, { ...cartItemToAdd, quantity: 1 }]; //appending the newItem to the cart
 };
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+	const existingCartItem = cartItems.find(
+		(cartItem) => cartItem.id === cartItemToRemove.id
+	);
+
+	if (existingCartItem.quantity === 1) {
+		return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id); //if the id is the same then remove the item
+	}
+
+	return cartItems.map(
+		(
+			cartItem //else decrease the quantity
+		) =>
+			cartItem.id === cartItemToRemove.id
+				? { ...cartItem, quantity: cartItem.quantity - 1 }
+				: cartItem
+	);
+};
