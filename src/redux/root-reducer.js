52e432894bf48fd6044536541bd7a6f0cb3b6110
AyariAla <alaaeddine.ayari1@esprit.tn>
2022-02-03 14:26:52 +0100

    implemented local storage persist.

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,19 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
 
+import storage from "redux-persist/lib/storage";
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 
-export default combineReducers({
+const persistConfig = {
+	key: "root",
+	storage,
+	whitelist: ["cart"], //passing the reducer we want to persist
+}; //user is handled by auth so we dont need to persist with reducer
+
+const rootReducer = combineReducers({
 	user: userReducer,
 	cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer); //returning a modified version of the root reducer w/ the persistconf
